e3b591a1660bbbb067e3e4b3f773421cdd9a6e1e
Allen Ng <allenng@bufalo.edu>
2017-08-12 15:21:23 -0400

    Cleaned up the code, still need a way to create a new dictionary every time it loops

--- pythonClient.py
+++ pythonClient.py
@@ -2,6 +2,7 @@
 import json
 import requests
 url = 'http://127.0.0.1:5000'
+# Names of the GitHub Usernames you want to search.
 dict = {'user-name-list': ['allengng', 'siushi']}
 data = json.dumps(dict)
 headers = {'Content-Type': 'application/json'}
--- pythonServer.py
+++ pythonServer.py
@@ -7,31 +7,21 @@ api = Api(app)
 
 class MyResource(Resource):
     def post(self):
-        """
-        #data = request.data
-        #dict = {'username1':'allengng', 'username2':'siushi'}
-        final = []
-        # Process data
-        for i in range (1,3):
-            f = requests.get('https://api.github.com/users/%s/keys' % dict.get('username%s' % i))
-            final.append(f.text)
 
-        return {'message': final}
-        """
         usernames = []
         keys = []
+
         # Load the data given by the client
         dict = json.loads(request.data)
         name = dict.get('user-name-list')
-        #for j in range(0, len(name)):
+
+        # The outline for the output back to the client
         dict2 = {'id': '', 'key': ''}
-        dict1 = {'name': '', 'id-key': dict2 }
+        dict1 = {'name': '', 'id-key': dict2}
         dict0 = {'user-keys' : dict1}
 
-
-
+        # Begin looping and get the ID and Key for the given GitHub usernames
         for i in range(0, len(name)):
-            #usernames.append(name[i])
             f = requests.get('https://api.github.com/users/%s/keys' % name[i])
 
             # Check to see if it was successful and returned somemthing
@@ -39,12 +29,13 @@ class MyResource(Resource):
                 dict1['name'] = name[i]
                 dictResult = f.json()
 
-                keys.append(f.text)
-                keys.append(f.status_code)
+                #keys.append(f.text)
+                #keys.append(f.status_code)
                 for i in dictResult:
                     dict2['id'] = i['id']
                     dict2['key'] = i['key']
-                usernames.append(dict0)
+
+                usernames.append(dict0.items())
         return {'message': usernames}
 
 api.add_resource(MyResource, '/')
