1534557fea99009cd51edcfab3471625dba42c5d
Allen Ng <allenng@bufalo.edu>
2017-08-11 23:05:26 -0400

    Added some code, kinda works but needs more work

--- pythonClient.py
+++ pythonClient.py
@@ -1,1 +1,8 @@
 # Creating the intial file for the client
+import json
+import requests
+url = 'http://127.0.0.1:5000'
+data = json.dumps({'fruit_1':'apple', 'fruit_2':'melon'})
+headers = {'Content-Type': 'application/json'}
+response = requests.post(url, data=data, headers=headers)
+print(response.json()['message'])
--- pythonServer.py
+++ pythonServer.py
@@ -1,1 +1,17 @@
-# Creating the server for the Python API Server
+from flask import Flask, request
+from flask_restful import Resource, Api
+import requests
+app = Flask(__name__)
+api = Api(app)
+
+class MyResource(Resource):
+    def post(self):
+        data = request.data
+        # Process data
+        f = requests.get('https://api.github.com/users/%s/keys' % data)
+        return {'message': f.text}
+
+api.add_resource(MyResource, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
