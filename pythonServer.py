5d83ef25aed8623505d299229ef1f2d41e3a7aa2
Allen Ng <allenng@bufalo.edu>
2017-08-13 11:56:54 -0400

    Got it working the way we wanted!

--- pythonServer.py
+++ pythonServer.py
@@ -10,53 +10,38 @@ api = Api(app)
 class MyResource(Resource):
     def post(self):
 
-        # Final list to be returned to Client.
-        usernames = []
 
         # Load the data given by the client,
-        dict = json.loads(request.data)
-        name = dict.get('user-name-list')
+        inputDict = json.loads(request.data)
+        nameList = inputDict.get('user-name-list')
 
-        # The outline for the output back to the client. NOT IN USE
-        #dict2 = {'id': '', 'key': ''}
-        #dict1 = {'name': '', 'id-key': dict2}
-        #dict0 = {'user-keys' : dict1}
-
-        dict6 = {}
-        listttt = []
+        outputDict = {}
+        dictList = []
 
         # Begin looping and get the ID and Key for the given GitHub usernames
-        for i in range(0, len(name)):
-            #dict5 = {'username': '', 'id': '', 'key': ''}
-
-
-            f = requests.get('https://api.github.com/users/%s/keys' % name[i])
-
-            # Regardless if the user has keys or IDs, set their usename.
-            #dict5['username'] = name[i]
+        for i in range(0, len(nameList)):
+            f = requests.get('https://api.github.com/users/%s/keys' % nameList[i])
 
             # Check to see if it was successful and returned something.
             if(f.status_code == requests.codes.ok):
-                dictResult = f.json()
+                responseData = f.json()
 
                 # Incase the user has multiple keys and IDs.
-                for j in dictResult:
-                    #dict5['id'] = j['id']
-                    #dict5['key'] = j['key']
-                    #usernames.append(dict5.items())
-                    dict6[name[i]] = [{'id': j['id'], 'key': j['key']}]
-                    #usernames.append(dict6[name[i]].items())
+                counter = 1
+                for j in responseData:
+                    if(counter == 1):
+                        outputDict[nameList[i]] = [{'id': j['id'], 'key': j['key']}]
+                    else:
+                        dictList = outputDict.get(nameList[i])
+                        dictList.append({'id': j['id'], 'key': j['key']})
+                    counter = counter + 1
 
             # If it does not return anything, fill in the blanks with 'N/A'.
             else:
-                dict6[name[i]] = [{'id': 'N/A', 'key': 'N/A'}]
-                #dict5['id'] = 'N/A'
-                #dict5['key'] = 'N/A'
-                #usernames.append(dict6.items())
+                outputDict[nameList[i]] = [{'id': 'N/A', 'key': 'N/A'}]
 
-        usernames.append(dict6.items())
         # Sends the list with all usernames, keys, and IDs to the client.
-        return {'message': usernames}
+        return {'message': outputDict}
 
 api.add_resource(MyResource, '/')
 
